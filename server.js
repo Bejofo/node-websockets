c4af9d4d25233e6e41a57d3afd200fbc1b89022b
Bejofo <32919220+Bejofo@users.noreply.github.com>
2018-11-01 11:07:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const SocketServer = require('ws').Server;
-const path = require('path');
+const WebSocket = require('ws');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+const wss = new WebSocket.Server({ port: 8080 });
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const wss = new SocketServer({ server });
-
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
-
-ws.on('message', (m) =>{
-  wss.clients.forEach((client) => {
-    client.send(m);
+// Broadcast to all.
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
   });
-})
+};
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    // Broadcast to everyone else.
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
