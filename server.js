fe59016778e5d2a1fbf39917de4dbc981281f43e
Bejofo <32919220+Bejofo@users.noreply.github.com>
2018-11-01 11:12:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const WebSocket = require('ws');
-
+const express = require('express');
+const path = require('path');
 const wss = new WebSocket.Server({ port: 8080 });
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
 
 // Broadcast to all.
 wss.broadcast = function broadcast(data) {
