ee1e4107084315e76ebdc8fe297885934de26ce2
Bejofo <32919220+Bejofo@users.noreply.github.com>
2018-11-01 12:32:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,17 +18,10 @@ wss.on('connection', (ws) => {
   ws.on('close', () => console.log('Client disconnected'));
 });
 
-wss.broadcast = function broadcast(data) {
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
-    }
-  });
-};
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
-    // Broadcast to everyone else.
+    console.log(`Data incoming :${data}`)
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
