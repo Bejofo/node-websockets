38caa33d6ef5d5cd0abd0db9a7612f9a87b3e6fb
Bejofo <32919220+Bejofo@users.noreply.github.com>
2018-11-01 17:03:03 +0800

    i think it works now

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
 const express = require('express');
+const WebSocket = require('ws');
+const SocketServer = WebSocket.Server;
 const path = require('path');
+
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
+
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
+const wss = new SocketServer({ server });
 
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8080 });
-// Broadcast to all.
-wss.broadcast = function broadcast(data) {
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
-    }
-  });
-};
 wss.on('connection', function connection(ws) {
+  console.log('WS opened');
   ws.on('message', function incoming(data) {
+    console.log(data);
     // Broadcast to everyone else.
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
