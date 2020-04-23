056a72f30da34332cc44b78860cbad354725d384
Bejofo <32919220+Bejofo@users.noreply.github.com>
2020-04-23 14:20:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,12 @@ const server = express()
 const wss = new SocketServer({ server });
 
 wss.on('connection', function connection(ws,req) {
-  console.log('WS opened' + ws.url);
+  console.log('WS opened: ' + ws.url);
   ws.on('message', function incoming(data) {
     wss.clients.forEach(function each(client) {
+	  if(ws.url == undefined || ws.url == null){
+		  console.log("Something fishy is going on. URL is null or undefined");
+	  }
       if (client !== ws && client.readyState === WebSocket.OPEN && client.url == ws.url) {
         client.send(data);
       }
